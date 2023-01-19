ed72224fa10f52d5d7b57b34c0277b38a4df8fa6
patteesolomon <patteesolomon@gmail.com>
2023-01-18 21:33:05 -0600

    todo done with more fashions

--- express-apt-14-main/server.js
+++ express-apt-14-main/server.js
@@ -1,36 +1,34 @@
-// Dependencies
-const express = require('express')
-const mongoose = require('mongoose')
-require('dotenv').config()
-const cors = require('cors')
-const db = mongoose.connection
+// Dependencies;
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const cors = require('cors');
+const db = mongoose.connection;
 const Todos = require('./models/todos.js');
 const todosData = require('./utilities/data');
 const todosController = require("./controllers/todos.js");
 
 // Environment Variables
 const app = express();
-const mongoURI = process.env.MONGODB_URI;
+const mongoURI = process.env.MONGO_URI;
 const PORT = process.env.PORT || 3001;
 
 // Connect to Mongo
 mongoose.connect(mongoURI, { useNewUrlParser: true },
   () => console.log('MongoDB connection established:', mongoURI)
-)
-
+);
 
 // Error / Disconnection
-db.on('error', err => console.log(err.message + ' is Mongod not running?'))
-db.on('disconnected', () => console.log('mongo disconnected'))
+db.on('error', err => console.log(err.message + ' is Mongod not running?'));
+db.on('disconnected', () => console.log('mongo disconnected'));
 
 // Middleware
-app.use(express.urlencoded({ extended: false }))// extended: false - does not allow nested objects in query strings
+app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
 app.use(express.json()); //use .json(), not .urlencoded()
-app.use(express.static('public')) // we need to tell express to use the public directory for static files... this way our app will find index.html as the route of the application! We can then attach React to that file!
-app.use(cors())
+app.use(express.static('public')); // we need to tell express to use the public directory for static files... this way our app will find index.html as the route of the application! We can then attach React to that file!
+app.use(cors());
 
 // Routes
-
 
 app.use('/todos', todosController);
 
@@ -42,5 +40,5 @@ app.get('/seed', async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(PORT, 'This message means nothing')
-})
+  console.log(PORT, 'This message means nothing');
+});
--- express-apt-14-main/utilities/data.js
+++ express-apt-14-main/utilities/data.js
@@ -2,6 +2,6 @@ const todoData = [
     {description: 'Eat Healthy', complete: false},
     {description: 'Work Out', complete: false},
     {description: 'Get Smart', complete: false}
-]
+];
 
 module.exports = todoData;
